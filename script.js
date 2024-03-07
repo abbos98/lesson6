583bca6b246cdec302648e16520ca861b32782a8
Abbos <abbos.pankiy2018@gmail.com>
2024-03-07 14:26:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////
+// obj update, delete, create //
+
+// let user = {
+//     name: 'John',
+//     surname: 'Smith',
+// }
+// user.name = 'Pete';
+// delete user.name;
+// console.log(user);
+
+///////////////////////////////////////////////
+
+// 2nd hometask //
+// let schedule = {
+//     name: 'Abbos'
+// };
+// let  schedule2 = {};    (false qilish holatini oxshata olmadim)
+
+// console.log(schedule);
+// console.log(schedule2);
+
+///////////////////////////////////////////////
+// 3rd hometask //
+// let salary = {
+//     John: 100,
+//     Ann: 160,
+//     Abbos: 130
+// }
+// let oylik = salary.John + salary.Ann + salary.Abbos;
+// let oylik2 = 0;
+// console.log(oylik) || console.log(oylik2);
+
+////////////////////////////////////////////////
+// 4th hometask //
+// function multiplyNumeric (obj) {
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'number') {
+//             obj[key] *= 2;
+//         }
+//     }
+// }
+// let obj = {
+//     width: 200,
+//     height : 300,
+//     title: "my menu"
+// };
+// multiplyNumeric(obj);
+// console.log(obj);
